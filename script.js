fb68e985ff3f6149701f537b8fea92a0da25a2bc
Fin-Cottler <fin.cottler@gmail.com>
2025-04-11 19:52:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,39 @@ function menuAnim() {
     }
 }
 
+function fileAnim() {
+    const frontcover = document.getElementById("front-cover");
+    const backcover = document.getElementById("back-cover");    
+    const grid = document.getElementById("tab-grid");
+    const paper = document.getElementById("page");
+
+
+
+
+    
+    frontcover.classList.toggle("open-front");
+    frontcover.classList.toggle("close-front");
+
+    backcover.classList.toggle("open-back");
+    backcover.classList.toggle("close-back");
+
+    grid.classList.toggle("open-back");
+    grid.classList.toggle("close-back");
+
+    paper.classList.toggle("open-page");
+    paper.classList.toggle("close-page");
+
+
+
+
+
+    
+
+
+    console.log(folder.classList)
+
+}
+
 setTimeout(function(){
     document.body.className="";
 },500);
